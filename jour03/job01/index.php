5ad1803849df2b19eee19cae36fdc984abf7a850
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-01-15 19:21:00 +0100

    jour03 job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Déclarer un tableau avec les nombres
+$nombres = [200, 204, 173, 98, 171, 404, 459];
+
+// Parcourir le tableau pour déterminer si chaque nombre est pair ou impair
+foreach ($nombres as $nombre) {
+    if ($nombre % 2 == 0) {
+        // Nombre pair
+        echo "$nombre est pair<br />";
+    } else {
+        // Nombre impair
+        echo "$nombre est impair<br />";
+    }
+}
+
+?>
